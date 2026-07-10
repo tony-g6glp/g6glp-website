645fb328de5b18fa730b76bab577dfac376e353e
Tony R <g6glp@strus.co.uk>
2026-07-10 10:50:04 +0100

    Document future role and permission system

--- include/auth.php
+++ include/auth.php
@@ -4,10 +4,32 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
+/*
+Future role/permission system
 
+Current roles:
+- admin
+
+Future roles may include:
+- superadmin
+- editor
+- author
+- contributor
+
+When multi-user support is added,
+permission checks should be handled here
+rather than hard-coded in individual pages.
+*/
 function require_login() {
     if (!isset($_SESSION['logged_in'])) {
         redirect('/admin/login.php');
+    }
+}
+
+function require_role($role)
+{
+    if (!isset($_SESSION['role']) || $_SESSION['role'] !== $role) {
+        die('Access denied');
     }
 }
 ?>
