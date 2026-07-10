6ca102cb342d393fd3f1c7aa8153d215f3624cd6
Tony R <g6glp@strus.co.uk>
2026-07-10 10:45:23 +0100

    Improve admin header with user information

--- include/header.php
+++ include/header.php
@@ -1,3 +1,44 @@
 <header class="site-header">
-    <h1>G6GLP Admin</h1>
+
+    <div class="header-left">
+
+        <h1>G6GLP CMS</h1>
+
+        <p>
+            Personal Website & Blog
+        </p>
+
+    </div>
+
+
+    <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']): ?>
+
+    <div class="header-user">
+
+        <strong>
+            <?= e($_SESSION['username']) ?>
+        </strong>
+
+        <br>
+
+        <span>
+            <?= e($_SESSION['role']) ?>
+        </span>
+
+        <br>
+
+        <a href="/g6glp/admin/profile.php">
+            Profile
+        </a>
+
+        |
+
+        <a href="/g6glp/admin/logout.php">
+            Logout
+        </a>
+
+    </div>
+
+    <?php endif; ?>
+
 </header>
