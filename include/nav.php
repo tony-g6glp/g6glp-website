c009969485f857e1d60bc808ad0a70a0d4e42897
Tony R <g6glp@strus.co.uk>
2026-07-11 17:13:03 +0100

    Update navigation to use permission checks

--- include/nav.php
+++ include/nav.php
@@ -4,20 +4,40 @@
 
 <a href="/g6glp/admin/">Dashboard</a>
 
+<?php if (can('manage_posts') || can('create_posts')): ?>
 <a href="/g6glp/admin/posts/">Posts</a>
+<?php endif; ?>
+
+<?php if (can('create_posts')): ?>
 <a href="/g6glp/admin/posts/new.php">New Post</a>
+<?php endif; ?>
 
+<?php if (can('manage_categories')): ?>
 <a href="/g6glp/admin/categories/">Categories</a>
-<a href="/g6glp/admin/tags/">Tags</a>
+<?php endif; ?>
 
-<a href="/g6glp/admin/media/">Media</a>
+<?php if (can('manage_tags')): ?>
+<a href="/g6glp/admin/tags/">Tags</a>
+<?php endif; ?>
+
+<?php if (
+    can('create_media') ||
+    can('delete_media')
+): ?>
+
+<a href="/g6glp/admin/media/">
+Media
+</a>
+
+<?php endif; ?>
 
 <a href="/g6glp/blog/">View Blog</a>
 
-
 <h4>Administration</h4>
 
+<?php if (can('manage_users')): ?>
 <a href="/g6glp/admin/users/">Users</a>
+<?php endif; ?>
 
 <a href="/g6glp/admin/change_password.php">
 Change Password
@@ -26,7 +46,6 @@ Change Password
 <a href="/g6glp/admin/profile.php">
 Profile
 </a>
-
 
 <a href="/g6glp/admin/logout.php">
 Logout
